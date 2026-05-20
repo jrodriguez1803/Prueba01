00a5f1a4d8aaf41dc551dbc5488d756c208c50a2
Jose Rodriguez <rodriguez.jose1803@gmail.com>
2026-05-20 14:55:50 -0400

    2do envio

---
+++ menu_principal.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Segunda prueba</title>
+</head>
+<body>
+    <div> Prueba de Git y GitHub</div>
+</body>
+</html>
